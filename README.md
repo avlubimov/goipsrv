b83cd34571e569739338fcb8c24ee54fbcb30231
AVL <avlubimov@gmail.com>
2013-12-16 12:57:42 +0400

    socket service init

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ goip server
 
 This is xinetd service for send and recieve sms trougth an GOIP gsm gateway.
 
+goip2 server
+
+This is socket service for send and recieve sms trougth an GOIP gsm gateway.
--- goipsrv2.lua
+++ goipsrv2.lua
@@ -1,24 +1,70 @@
 #!/usr/bin/lua
 
+--comment this line if you don't have local C libs
+LOCAL_CPATH="./clibs/?.so;"
+--comment this line if you don't have local lua libs
+--LOCAL_PATH="./clibs/?.so;"
+
+DEBUG=true
+
+if LOCAL_CPATH then package.cpath=LOCAL_CPATH..package.cpath end
+if LOCAL_PATH then package.path=LOCAL_PATH..package.path end
+
+print (package.cpath)
 
 local socket = require("socket")
+local syslog = require "lsyslog"
 
 
-local socket = require("socket")
 host = "172.64.10.90"
 port = 44444
-udp = assert(socket.udp())
-assert(udp:setsockname(host, port))
-assert(udp:settimeout(5))
-ip, port = udp:getsockname()
-assert(ip, port)
-print("Waiting packets on " .. ip .. ":" .. port .. "...")
 
-while 1 do
-    dgram, ip, port = udp:receivefrom()
-    if dgram then
-	print("Echoing '" .. dgram .. "' to " .. ip .. ":" .. port)
-    else
-	print(ip)
+
+--init
+
+function init_service ()
+
+    syslog.open("goipsrv", syslog.FACILITY_USER)
+
+    --bind
+    netsock = assert(socket.udp())
+    assert(netsock:setsockname(host, port))
+    assert(netsock:settimeout(2))
+    local ip, port = netsock:getsockname()
+    assert(ip, port)
+
+    if DEBUG then syslog.log(syslog.LOG_INFO,("Waiting packets on %s:%s"):format(ip, port)) end
+end 
+
+
+function close_service ()
+    syslog.close()
+end
+
+function process_request (request)
+    if request["req"] then
+	for key,value in pairs(request) do
+	    print (key,value)
+	end
     end
 end
+
+
+
+
+init_service()
+
+while 1 do
+    dgram, ip, port = netsock:receivefrom()
+    if dgram then
+	print("Echoing '" .. dgram .. "' to " .. ip .. ":" .. port)
+	local request={}
+	for key,value in dgram:gfind("([^:;]+):([^:;]+);") do
+	    request[key]=value
+	end
+	process_request(request)
+    else
+	print ("else")
+    end
+end
+
